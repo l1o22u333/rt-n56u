ec803b7d2979f9346494c4d1b2d65ee6026d3412
l1o22u333 <l1o22u333@gmail.com>
2026-06-09 04:26:06 +0000

    Update firmware source from Docker

--- trunk/proprietary/rt_wifi/rtpci/5.0.4.0/mt7615/embedded/hw_ctrl/hdev/wmm_ctrl.c
+++ trunk/proprietary/rt_wifi/rtpci/5.0.4.0/mt7615/embedded/hw_ctrl/hdev/wmm_ctrl.c
@@ -232,7 +232,7 @@ VOID WcSetEdca(struct hdev_obj *obj)
 		pEdca = WcGetWmmByIdx(ctrl, obj->WmmIdx);
 		wdev = pAd->wdev_list[obj->Idx];
 	/* == 加入實驗性修改：僅針對 5GHz Client / APCLI 觸發惡霸模式 == */
-		if (wdev && wdev->wdev_type == WDEV_TYPE_STA) {
+		if (wdev && wdev->wdev_type == WDEV_TYPE_APCLI) {
 			if (wdev->channel > 14) { // 頻道大於 14 代表是 5GHz
 				INT ac_idx;
 				for (ac_idx = 0; ac_idx < 4; ac_idx++) {
--- trunk/user/shared/board.mk
+++ trunk/user/shared/board.mk
@@ -1,8 +1,8 @@
 ##################################################################
 # Board PID # Board Name       # PRODUCT # Note
 ##################################################################
-# RT-N56U   # ASUS RT-N56U     # RT3883  #
+# Redmi-AC2100 # Redmi-AC2100   # MT7621  #
 ##################################################################
 
-CFLAGS += -DBOARD_N56U -DVENDOR_ASUS
-BOARD_NUM_USB_PORTS=2
+CFLAGS += -DBOARD_RM2100
+BOARD_NUM_USB_PORTS=0
